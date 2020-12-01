676b1dca86dac0569613c81100d6f293ff504ba9
Lucas Fabreguettes <lucas31ff@gmail.com>
2020-12-01 18:40:33 +0100

    feat: Add the possibility to add an element to the card and to delete it

--- src/screens/CardPage.tsx
+++ src/screens/CardPage.tsx
@@ -9,7 +9,7 @@ import {
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator, HeaderBackButton } from "@react-navigation/stack";
 import { Dish } from '../models/dish'
 
 const { height } = Dimensions.get("window");
@@ -34,36 +34,53 @@ const styles = StyleSheet.create({
       marginLeft: 10,
     },
     categoryStyle: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
       borderBottomWidth: 0.5,
       marginRight: 13,
       borderBottomColor: "#C6C6C8",
-      justifyContent: "center",
       height: 35
     },
     cardCategoryContainer: {
       display: "flex",
       flexDirection: "column"
-    }
+    },
+    categoryAppend: {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      marginTop: 10
+    },
+    textAdd: {
+      marginLeft: 10,
+      color: "#9A9A9A",
+      fontSize: 15
+    },
   }
 );
 
 type CardCategoryProps = {
   title: string;
   type: string;
+  category: string;
 };
 
 const CARD_CATEGORY: CardCategoryProps[] = [
   {
     title: "Mes entrées:",
     type: "starter",
+    category: "une entrée"
   },
   {
     title: "Mes plats:",
-    type: "plate"
+    type: "plate",
+    category: "un plat"
   },
   {
     title: "Mes desserts:",
-    type: "dessert"
+    type: "dessert",
+    category: "un dessert"
   }
 ];
 
@@ -71,6 +88,63 @@ type Props = {
   props: CardCategoryProps,
   list: Dish[]
 }
+
+const DISH_LIST: Dish[] = [
+  {
+    id: "1",
+    name: "Carpaccio",
+    type: "starter",
+    description: "Un super Carpaccio",
+    price: "7",
+    ingredients: [],
+    sauces: [],
+  },
+  {
+    id: "2",
+    name: "Salade de concombre",
+    type: "starter",
+    description: "La super salade",
+    price: "10",
+    ingredients: [],
+    sauces: [],
+  },
+  {
+    id: "3",
+    name: "Fondant au chocolat",
+    type: "dessert",
+    description: "Le meilleur fondant du monde",
+    price: "100",
+    ingredients: [],
+    sauces: [],
+  },
+  {
+    id: "4",
+    name: "Entrecôte de boeuf",
+    type: "plate",
+    description: "Une entrecôte saignante",
+    price: "100",
+    ingredients: [],
+    sauces: [],
+  },
+  {
+    id: "5",
+    name: "Pâtes bolognaise",
+    type: "plate",
+    description: "Des pâtes bolognaises",
+    price: "100",
+    ingredients: [],
+    sauces: [],
+  },
+  {
+    id: "6",
+    name: "Glace à la vanille",
+    type: "dessert",
+    description: "Une super glace",
+    price: "100",
+    ingredients: [],
+    sauces: [],
+  }
+]
 
 const CardCategory : FC<Props> = ({props, list}: Props) => {
   return (
@@ -80,75 +154,25 @@ const CardCategory : FC<Props> = ({props, list}: Props) => {
         return (
           <View style={styles.categoryStyle} key={dish.id}>
             <Text style={styles.dishName}>{dish.name}</Text>
+            <AntDesign name="close" size={18} color="#C6C6C8" />
           </View>
         )
       })}
+      <View style={styles.categoryAppend}>
+        <AntDesign name="plus" size={20} color='#2196F3' />
+        <Text style={styles.textAdd}>Ajouter {props.category}</Text>
+      </View>
     </View>
   );
 };
 
 const CardPage : FC = () => {
-  let firstDish: Dish = {
-    id: "1",
-    name: "Carpaccio",
-    type: "starter",
-    description: "Un super Carpaccio",
-    price: "7",
-    ingredients: [],
-    sauces: [],
-  };
-  let secondDish: Dish = {
-    id: "2",
-    name: "Salade de concombre",
-    type: "starter",
-    description: "La super salade",
-    price: "10",
-    ingredients: [],
-    sauces: [],
-  };
-  let thirdDish: Dish = {
-    id: "3",
-    name: "Fondant au chocolat",
-    type: "dessert",
-    description: "Le meilleur fondant du monde",
-    price: "100",
-    ingredients: [],
-    sauces: [],
-  };
-  let fourthDish: Dish = {
-    id: "4",
-    name: "Entrecôte de boeuf",
-    type: "plate",
-    description: "Une entrecôte saignante",
-    price: "100",
-    ingredients: [],
-    sauces: [],
-  };
-  let fifthDish: Dish = {
-    id: "5",
-    name: "Pâtes bolognaise",
-    type: "plate",
-    description: "Des pâtes bolognaises",
-    price: "100",
-    ingredients: [],
-    sauces: [],
-  };
-  let sixthDish: Dish = {
-    id: "6",
-    name: "Glace à la vanille",
-    type: "dessert",
-    description: "Une super glace",
-    price: "100",
-    ingredients: [],
-    sauces: [],
-  };
-  let list = [firstDish, secondDish, thirdDish, fourthDish, fifthDish]
   return (
     <View style={styles.generalStyle}>
       {CARD_CATEGORY.map((card) => {
         return (
           <View style={styles.cardTypeDescription} key={card.title}>
-            <CardCategory props={card} list={list.filter(dish => dish.type == card.type)}/>
+            <CardCategory props={card} list={DISH_LIST.filter(dish => dish.type == card.type)}/>
           </View>
         );
       })}
@@ -167,9 +191,18 @@ export default () => {
             shadowOffset: { width: 0, height: 2 },
             shadowRadius: 2,
           },
+          headerTitleStyle: {
+            fontSize: 18
+          }
         }}
       >
-        <Stack.Screen name="Ma carte" component={CardPage} />
+        <Stack.Screen name="Ma carte" component={CardPage} options={{
+          headerLeft: () => (
+            <AntDesign style={{paddingLeft: 20}} name="arrowleft" size={25} color={"black"}
+            onPress={() => alert("Todo")}/>
+          ),
+        }}
+        ></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
