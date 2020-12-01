8056fa915657928ed3189aed6559a242323c5d98
mattéo <fauchonmatteo@gmail.com>
2020-12-01 17:13:35 +0100

    feat: add header image instead of title, add default image for a restaurant

---
+++ src/components/home/restaurant-button.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import {
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  Image,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+const { height } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  restaurantButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+    height: 80,
+    borderBottomWidth: 0.5,
+    borderBottomColor: "#C6C6C8",
+    marginBottom: height / 10,
+  },
+  restaurantButtonContext: {
+    flexDirection: "row",
+  },
+  imageContainer: {
+    height: 60,
+    width: 60,
+    borderRadius: 60 / 2,
+  },
+});
+
+const RestaurantButton = () => {
+  return (
+    <TouchableOpacity
+      activeOpacity={1}
+      style={styles.restaurantButton}
+      onPress={() => alert("Todo!")}
+    >
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <Image
+          source={require("../../../assets/images/Memphis.jpg")}
+          style={styles.imageContainer}
+        />
+        <View
+          style={{
+            flexDirection: "column",
+            justifyContent: "space-between",
+            height: 75,
+            paddingVertical: 10,
+            paddingLeft: 18,
+          }}
+        >
+          <Text style={{ fontSize: 17 }}>Mon restaurant</Text>
+          <Text style={{ fontSize: 13, color: "#9A9A9A" }}>
+            Nom, adresse, numéro, horaires
+          </Text>
+        </View>
+      </View>
+      <AntDesign name="right" size={15} color="#C6C6C8" />
+    </TouchableOpacity>
+  );
+};
+
+export default RestaurantButton;
--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,54 +1,16 @@
-import React from "react";
-import {
-  View,
-  TouchableOpacity,
-  StyleSheet,
-  Text,
-  Image,
-  Dimensions,
-} from "react-native";
-import { AntDesign } from "@expo/vector-icons";
+import React, { FC } from "react";
+import { View, StyleSheet, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-
-const { height } = Dimensions.get("window");
+import RestaurantButton from "../components/home/restaurant-button";
+import Button, { ButtonProps } from "../components/home/button";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
   },
-  restaurantButton: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingHorizontal: 16,
-    height: 80,
-    borderBottomWidth: 0.5,
-    borderBottomColor: "#C6C6C8",
-    marginBottom: height / 10,
-  },
-  restaurantButtonContext: {
-    flexDirection: "row",
-  },
-  button: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingHorizontal: 16,
-    height: 60,
-    borderTopWidth: 0.5,
-    borderTopColor: "#C6C6C8",
-  },
-  buttonText: {
-    fontSize: 17,
-  },
 });
-
-type ButtonProps = {
-  id: string;
-  label: string;
-};
 
 const BUTTONS: ButtonProps[] = [
   {
@@ -65,49 +27,7 @@ const BUTTONS: ButtonProps[] = [
   },
 ];
 
-const Button = ({ id, label }: ButtonProps) => {
-  return (
-    <TouchableOpacity
-      activeOpacity={1}
-      style={styles.button}
-      onPress={() => alert("Todo!")}
-    >
-      <Text style={styles.buttonText}>{label}</Text>
-      <AntDesign name="right" size={15} color="#C6C6C8" />
-    </TouchableOpacity>
-  );
-};
-
-const RestaurantButton = () => {
-  return (
-    <TouchableOpacity
-      activeOpacity={1}
-      style={styles.restaurantButton}
-      onPress={() => alert("Todo!")}
-    >
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <Text>Image</Text>
-        <View
-          style={{
-            flexDirection: "column",
-            justifyContent: "space-between",
-            height: 75,
-            paddingVertical: 10,
-            paddingLeft: 18,
-          }}
-        >
-          <Text style={{ fontSize: 17 }}>Mon restaurant</Text>
-          <Text style={{ fontSize: 13, color: "#9A9A9A" }}>
-            Nom, adresse, numéro, horaires
-          </Text>
-        </View>
-      </View>
-      <AntDesign name="right" size={15} color="#C6C6C8" />
-    </TouchableOpacity>
-  );
-};
-
-const HomePage = () => {
+const HomePage: FC = () => {
   return (
     <View style={styles.container}>
       <RestaurantButton />
@@ -127,7 +47,7 @@ const HomePage = () => {
 
 const Stack = createStackNavigator();
 
-export default () => {
+const Home: FC = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -138,8 +58,21 @@ export default () => {
           },
         }}
       >
-        <Stack.Screen name="Home" component={HomePage} />
+        <Stack.Screen
+          name="Home"
+          component={HomePage}
+          options={{
+            headerTitle: () => (
+              <Image
+                source={require("../../assets/images/Shoku.png")}
+                style={{ width: 135, height: 36 }}
+              />
+            ),
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
+export default Home;
