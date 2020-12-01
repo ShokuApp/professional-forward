33cf08f9678244c0be300c64bcac8c2c41159e9a
Anthonin Clara <anthonin.clara@epitech.eu>
2020-12-01 16:41:40 +0100

    feat: add the cardcategory list

--- src/screens/CardPage.tsx
+++ src/screens/CardPage.tsx
@@ -10,12 +10,61 @@ import {
 import { AntDesign } from "@expo/vector-icons";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Dish } from '../models/dish'
 
 const { height } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+    cardCategoryType: {
+      color: '#2196F3',
+      fontSize: 14
+    }
+  }
+);
+
+type CardCategoryProps = {
+  title: string;
+  type: string;
+};
+
+const CARD_CATEGORY: CardCategoryProps[] = [
+  {
+    title: "Mes entrées",
+    type: "starter",
+  },
+  {
+    title: "Mes plats",
+    type: "plate"
+  },
+  {
+    title: "Mes desserts",
+    type: "dessert"
+  }
+];
+
+type Props = {
+  props: CardCategoryProps,
+  list: Dish[]
+}
+
+const CardCategory = ({props, list}: Props) => {
+  return (
+    <View>
+      <Text>{props.title}</Text>
+    </View>
+  );
+};
 
 const CardPage = () => {
   return (
     <View>
+      {CARD_CATEGORY.map((card) => {
+        return (
+          <View key={card.title}>
+            <CardCategory props={card} list={[]}/>
+          </View>
+        );
+      })}
     </View>
   );
 };
