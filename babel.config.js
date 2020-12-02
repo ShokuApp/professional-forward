ad5c9240cb9b35f7934a29f0c107505e9539561d
mattéo <fauchonmatteo@gmail.com>
2020-12-02 16:53:33 +0100

    feat: add babel plugin

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
+      "@babel/plugin-proposal-async-generator-functions",
       [
         "module-resolver",
         {
