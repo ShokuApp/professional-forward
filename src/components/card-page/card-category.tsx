26037cba55647e329928ba08ea82f9dab99fc855
mattéo <fauchonmatteo@gmail.com>
2020-12-02 17:19:34 +0100

    feat: add alert when the user wants to delete a dish

--- src/components/card-page/card-category.tsx
+++ src/components/card-page/card-category.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Alert } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
-import { Card } from "../../models/";
+import { Card, Dish } from "../../models/";
 import CardText from "./card-text";
 import AppendCategory from "./append-category";
 import { CardBloc, CardSetEvent } from "../../blocs";
@@ -30,12 +30,31 @@ type Props = {
   card: Card;
 };
 
-const deleteDish = (cardBloc: CardBloc, card: Card, dishId: string) => {
-  card.dishes = card.dishes.filter((dish) => dish.id !== dishId);
-  cardBloc.add(new CardSetEvent(card.id, card));
-};
-
 const CardCategory: FC<Props> = ({ cardBloc, props, card }: Props) => {
+  const deleteDish = (dishId: string) => {
+    card.dishes = card.dishes.filter((dish) => dish.id !== dishId);
+    cardBloc.add(new CardSetEvent(card.id, card));
+  };
+
+  const alertConfirmation = (dish: Dish) => {
+    Alert.alert(
+      dish.name,
+      "Supprimer ce plat de la carte ?",
+      [
+        {
+          text: "Supprimer",
+          onPress: () => deleteDish(dish.id),
+        },
+        {
+          text: "Cancel",
+          onPress: () => null,
+          style: "cancel",
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
   return (
     <View style={styles.cardCategoryContainer}>
       <Text style={styles.cardCategoryType}>{props.title}</Text>
@@ -52,7 +71,7 @@ const CardCategory: FC<Props> = ({ cardBloc, props, card }: Props) => {
                     name={"close"}
                     size={18}
                     color="#C6C6C8"
-                    onPress={() => deleteDish(cardBloc, card, dish.id)}
+                    onPress={() => alertConfirmation(dish)}
                   />
                 }
               />
