38ea7133910067ec863680b69b502252dd3f43ca
Lucas Fabreguettes <lucas31ff@gmail.com>
2020-12-04 14:48:16 +0100

    feat: Add the type of the plate in the add plate screen

---
+++ src/components/plates/add-plate/plate-type.tsx
@@ -0,0 +1,45 @@
+import React, { FC, SetStateAction } from "react";
+import { View, StyleSheet, Picker, ActionSheetIOS, Button } from "react-native";
+import CategoryTitle from "../../common/category-title";
+import DropDownPicker from "react-native-dropdown-picker";
+import Icon from "react-native-vector-icons/Feather";
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 30,
+  },
+  containerPicker: {
+    height: 40,
+  },
+  picker: {
+    marginTop: 5,
+    backgroundColor: "white",
+  },
+});
+
+type PlateTypeProps = {
+  type: string;
+  setType: React.Dispatch<SetStateAction<string>>;
+};
+
+const PlateType: FC<PlateTypeProps> = ({ type, setType }: PlateTypeProps) => {
+  return (
+    <View style={styles.container}>
+      <CategoryTitle label="Type du plat:" />
+      <DropDownPicker
+        items={[
+          { label: "Entrée", value: "starter" },
+          { label: "Plat", value: "plate" },
+          { label: "Dessert", value: "dessert" },
+        ]}
+        defaultValue="plate"
+        containerStyle={styles.containerPicker}
+        style={styles.picker}
+        dropDownStyle={styles.picker}
+        onChangeItem={(item) => setType(item.value)}
+      />
+    </View>
+  );
+};
+
+export default PlateType;
--- src/screens/plates/add-plate.tsx
+++ src/screens/plates/add-plate.tsx
@@ -1,11 +1,26 @@
 import React, { FC } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import PlateName from "../../components/plates/add-plate/plate-name";
+import PlateType from "../../components/plates/add-plate/plate-type";
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "white",
+  },
+});
 
 const AddPlate: FC = () => {
-  const [plateName, setPlateName] = React.useState(" ");
-
-  return <PlateName name={plateName} setName={setPlateName} />;
+  const [plateName, setPlateName] = React.useState("");
+  const [plateType, setPlateType] = React.useState("plate");
+  return (
+    <View style={styles.container}>
+      <PlateName name={plateName} setName={setPlateName} />
+      <PlateType type={plateType} setType={setPlateType} />
+    </View>
+  );
 };
 
 export default AddPlate;
